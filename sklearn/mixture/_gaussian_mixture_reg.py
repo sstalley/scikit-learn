6847920c601f8d8e2e9cd5eb964d2d0e58877ab5
Sean O. Stalley <sstalley@pdx.edu>
2020-07-15 12:25:39 -0700

    Added Spectral Clustering-based Initialization to our laplacian-regularized gmm model
    Also removed a bunch of dead/commented out code

--- sklearn/mixture/_gaussian_mixture_reg.py
+++ sklearn/mixture/_gaussian_mixture_reg.py
@@ -8,6 +8,7 @@ from ._base import _check_X
 from ._gaussian_mixture import GaussianMixture
 from ..utils import check_random_state
 from ..utils.validation import _deprecate_positional_args
+from ..cluster import SpectralClustering
 
 def _lap_reg(prob, laplacian):
     """calculate the laplacian regularizer
@@ -85,6 +86,34 @@ class LapRegGaussianMixture(GaussianMixture):
 
         self.similarity = similarity
 
+    # Overloading to add spectral clustering initialization
+    def _initialize_parameters(self, X, random_state):
+        """Initialize the model parameters.
+
+        Parameters
+        ----------
+        X : array-like of shape  (n_samples, n_features)
+
+        random_state : RandomState
+            A random number generator instance that controls the random seed
+            used for the method chosen to initialize the parameters.
+        """
+        n_samples, _ = X.shape
+
+        if self.init_params == 'spectral':
+            print("initializing using spectral clustering...")
+            resp = np.zeros((n_samples, self.n_components))
+            label = SpectralClustering(n_clusters=self.n_components, affinity='precomputed',
+                                   random_state=random_state).fit(self.similarity).labels_
+            resp[np.arange(n_samples), label] = 1
+            print(np.argmax(resp, axis=1))
+            self._initialize(X, resp)
+        else:
+            super()._initialize_parameters(X, random_state)
+
+
+
+    # Overloading this function from the 'base' model to add smoothing
     def fit_predict(self, X, y=None):
         """Estimate model parameters using X and predict the labels for X.
 
@@ -206,74 +235,10 @@ class LapRegGaussianMixture(GaussianMixture):
 
         return log_resp.argmax(axis=1)
 
-    # Overloading this function from the 'base' model to add smoothing
     def _e_step(self, X):
-        """E step.
-
-        Parameters
-        ----------
-        X : array-like of shape (n_samples, n_features)
-
-        Returns
-        -------
-        log_prob_norm : float
-            Mean of the logarithms of the probabilities of each sample in X
-
-        log_responsibility : array, shape (n_samples, n_components)
-            Logarithm of the posterior probabilities (or responsibilities) of
-            the point of each sample in X.
-        """
         # This shouldn't ever get used anymore, but if it does we want to know
         # about it and scream as loudly as we can
         assert False
-#        # HACK: shadow the lower-bound so we know what it is without looking
-#        lower_bound = self.lower_bound_HAX
-#        orig_lower_bound = lower_bound
-#        prev_lower_bound = lower_bound
-#
-#        log_prob_norm_orig, log_resp_orig = self._estimate_log_prob_resp(X)
-#
-#        (n_samples, n_component) = log_resp_orig.shape
-#
-#
-#        while self.lap_smooth > self.lap_tol:
-#
-#            #smooth here
-#            reg2 = _lap_reg_2(np.exp(log_resp_orig), self.similarity)
-#            log_resp = (1 - self.lap_smooth) * log_resp_orig + self.lap_smooth * reg2
-#            assert (n_samples, n_component) == log_resp.shape
-#
-#            # update the log_prob_norm (since we updated the probabilities)
-#            log_prob_norm, _ = self._estimate_log_prob_resp(X)
-#        
-#            # print("log_resp.shape", log_resp.shape)
-#            # print("X.shape", X.shape)
-#
-#            # HACK - we call this here so we can make sure our values are geud
-#            # This will be immediately be called again when this function returns
-#            self._m_step(X, log_resp)
-#
-#            lower_bound = self._compute_lower_bound(log_resp, np.mean(log_prob_norm))
-#
-#            change = lower_bound - prev_lower_bound
-#
-#            if change >= 0:
-#                break
-#
-#            # If we didn't get better, apply more smoothing and try again
-#            self.lap_smooth = self.lap_smooth * self.lap_reduce
-#            print("lap_smooth %.5f ll change %.5f" % (self.lap_smooth, change))
-#
-#
-#        # If we couldn't make it better, return what we had originally
-#        if self.lap_smooth <= self.lap_tol:
-#            log_prob_norm = log_prob_norm_orig
-#            log_resp = log_resp_orig
-#            lower_bound = orig_lower_bound
-#
-#        self.lower_bound_HAX = lower_bound
-#
-#        return np.mean(log_prob_norm), log_resp
 
     def _compute_lower_bound(self, log_resp, log_prob_norm):
         return log_prob_norm - self.lap_mag * _lap_reg(np.exp(log_resp), self.laplacian)
